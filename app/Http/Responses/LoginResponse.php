6fcdf580e6174c8580ddd9e49c5f90422b8eb60e
PanapatWonganan <abadoned@gmail.com>
2025-11-30 00:38:48 +0700

    Remove return type from LoginResponse to support Livewire redirector

--- app/Http/Responses/LoginResponse.php
+++ app/Http/Responses/LoginResponse.php
@@ -3,19 +3,17 @@
 namespace App\Http\Responses;
 
 use Filament\Http\Responses\Auth\Contracts\LoginResponse as LoginResponseContract;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Redirect;
 
 class LoginResponse implements LoginResponseContract
 {
-    public function toResponse($request): RedirectResponse
+    public function toResponse($request)
     {
         // ถ้ามี company_id ใน session แล้ว ให้ไปหน้า admin
         if (session('company_id')) {
-            return Redirect::intended(filament()->getUrl());
+            return redirect()->intended(filament()->getUrl());
         }
 
         // ถ้ายังไม่มี ให้ไปหน้าเลือกบริษัทก่อน (ใน Filament Panel)
-        return Redirect::to('/admin/company-select');
+        return redirect('/admin/company-select');
     }
 }
