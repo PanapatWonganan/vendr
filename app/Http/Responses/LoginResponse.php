0cebafdcc55d9c9d5c275860c2d46325b91ea69a
PanapatWonganan <abadoned@gmail.com>
2025-11-30 00:37:45 +0700

    Fix LoginResponse to use Redirect facade instead of helper

--- app/Http/Responses/LoginResponse.php
+++ app/Http/Responses/LoginResponse.php
@@ -4,6 +4,7 @@ namespace App\Http\Responses;
 
 use Filament\Http\Responses\Auth\Contracts\LoginResponse as LoginResponseContract;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Redirect;
 
 class LoginResponse implements LoginResponseContract
 {
@@ -11,10 +12,10 @@ class LoginResponse implements LoginResponseContract
     {
         // ถ้ามี company_id ใน session แล้ว ให้ไปหน้า admin
         if (session('company_id')) {
-            return redirect()->intended(filament()->getUrl());
+            return Redirect::intended(filament()->getUrl());
         }
 
         // ถ้ายังไม่มี ให้ไปหน้าเลือกบริษัทก่อน (ใน Filament Panel)
-        return redirect('/admin/company-select');
+        return Redirect::to('/admin/company-select');
     }
 }
